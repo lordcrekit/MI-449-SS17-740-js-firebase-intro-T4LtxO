87401ea1048d4c61717b5e8e2fa9085075110099
William Norman <lordcrekit@gmail.com>
2017-04-19 17:41:31 -0400

    Added project starter files from prof

---
+++ woofer-db.js
@@ -0,0 +1,25 @@
+// TODO Sign into the database anonymously
+
+// CREATE a new woof in Firebase
+function createWoofInDatabase (woof) {
+  // TODO create a new record in Firebase
+}
+
+// READ from Firebase when woofs are added, changed, or removed
+// Call addWoofRow, updateWoofRow, and deleteWoofRow to update the page
+function readWoofsInDatabase () {
+  // TODO read new, changed, and deleted Firebase records
+}
+
+// UPDATE the woof in Firebase
+function updateWoofInDatabase (woofKey, woofText) {
+  // TODO update the record in Firebase
+}
+
+// DELETE the woof from Firebase
+function deleteWoofFromDatabase (woofKey) {
+  // TODO delete the record from Firebase
+}
+
+// Load all of the data
+readWoofsInDatabase()
