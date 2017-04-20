04fa82bab97037989c628fa05381100b5ca57ff0
William Norman <lordcrekit@gmail.com>
2017-04-20 16:43:04 -0400

    Now passes linting

--- woofer-db.js
+++ woofer-db.js
@@ -1,27 +1,52 @@
-// TODO Sign into the database anonymously
+/* global firebase */
 
-var wooferdb = { // eslint-disable-line no-unused-vars
-  // CREATE a new woof in Firebase
-  createWoofInDatabase: function (woof) {
-    // TODO create a new record in Firebase
-  },
-
-  // READ from Firebase when woofs are added, changed, or removed
-  // Call addWoofRow, updateWoofRow, and deleteWoofRow to update the page
-  readWoofsInDatabase: function () {
-    // TODO read new, changed, and deleted Firebase records
-  },
-
-  // UPDATE the woof in Firebase
-  updateWoofInDatabase: function (woofKey, woofText) {
-    // TODO update the record in Firebase
-  },
-
-  // DELETE the woof from Firebase
-  deleteWoofFromDatabase: function (woofKey) {
-    // TODO delete the record from Firebase
+(function () {
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyC5wJiDYv-stmfG3l5LDqUHX7_zAYc6ZAc',
+    authDomain: 'js-firebase-intro.firebaseapp.com',
+    databaseURL: 'https://js-firebase-intro.firebaseio.com',
+    projectId: 'js-firebase-intro',
+    storageBucket: 'js-firebase-intro.appspot.com',
+    messagingSenderId: '572883060629'
   }
-}
+  firebase.initializeApp(config)
 
-// Load all of the data
-wooferdb.readWoofsInDatabase()
+  // Sign into the database anonymously
+  firebase.auth().signInAnonymously()
+
+  // Create and export wooferdb
+  var wooferdb = {
+    // CREATE a new woof in Firebase
+    createWoofInDatabase: function (woof) {
+      window.alert(JSON.stringify(woof))
+      firebase.ref('woofs').push(woof)
+    },
+
+    // If we are already reading from the database.
+    _reading: false,
+
+    // READ from Firebase when woofs are added, changed, or removed
+    // Call addWoofRow, updateWoofRow, and deleteWoofRow to update the page
+    readWoofsInDatabase: function () {
+      if (wooferdb._reading) {
+      }
+      // TODO read new, changed, and deleted Firebase records
+    },
+
+    // UPDATE the woof in Firebase
+    updateWoofInDatabase: function (woofKey, woofText) {
+      // TODO update the record in Firebase
+    },
+
+    // DELETE the woof from Firebase
+    deleteWoofFromDatabase: function (woofKey) {
+      // TODO delete the record from Firebase
+    }
+  }
+
+  window.wooferdb = wooferdb
+
+  // Load all of the data
+  wooferdb.readWoofsInDatabase()
+})()
--- woofer-ui.js
+++ woofer-ui.js
@@ -1,4 +1,4 @@
-/* global wooferdb */
+/* global wooferdb, moment */
 
 //
 //
@@ -10,92 +10,98 @@
 //
 //
 
-var woofText = document.getElementById('woof-text')
-var woofs = document.getElementById('woofs')
-var woofCreate = document.getElementById('woof-button')
+(function () {
+  var woofText = document.getElementById('woof-text')
+  var woofs = document.getElementById('woofs')
+  var woofCreate = document.getElementById('woof-button')
 
-// Adds a new row to the list of woofs
-function addWoofRow (woofKey, woof) {
-  var template = document.getElementById('woof-template')
-  var clone = document.importNode(template.content, true)
+  // Adds a new row to the list of woofs
+  function addWoofRow (woofKey, woof) {
+    var template = document.getElementById('woof-template')
+    var clone = document.importNode(template.content, true)
 
-  clone.querySelector('.row').id = woofKey
-  clone.querySelector('.created-at').textContent = moment(woof.created_at).calendar()
-  clone.querySelector('.text').textContent = woof.text
-  clone.querySelector('.btn-edit').addEventListener('click', showWoofTextbox)
-  clone.querySelector('.btn-delete').addEventListener('click', deleteWoof)
-  clone.querySelector('input').addEventListener('keyup', editWoof)
+    clone.querySelector('.row').id = woofKey
+    clone.querySelector('.created-at').textContent = moment(woof.created_at).calendar()
+    clone.querySelector('.text').textContent = woof.text
+    clone.querySelector('.btn-edit').addEventListener('click', showWoofTextbox)
+    clone.querySelector('.btn-delete').addEventListener('click', deleteWoof)
+    clone.querySelector('input').addEventListener('keyup', editWoof)
 
-  woofs.insertBefore(clone, woofs.firstChild)
-  woofText.value = ''
-}
+    woofs.insertBefore(clone, woofs.firstChild)
+    woofText.value = ''
+  }
 
-// Get woof text from input and pass it to addWoof
-function createWoof () {
-  var text = woofText.value || ''
-  if (!text.trim().length) return
-  wooferdb.createWoofInDatabase({
-    created_at: new Date().getTime(),
-    text: text
-  })
-}
+  // Get woof text from input and pass it to addWoof
+  function createWoof () {
+    var text = woofText.value || ''
+    if (!text.trim().length) return
+    wooferdb.createWoofInDatabase({
+      created_at: new Date().getTime(),
+      text: text
+    })
+  }
 
-// Make the textbox to edit a woof appear
-function showWoofTextbox () {
-  var row = this.parentElement.parentElement
-  var textbox = row.querySelector('input')
-  var form = textbox.parentElement
-  var text = row.querySelector('.text')
+  // Make the textbox to edit a woof appear
+  function showWoofTextbox () {
+    var row = this.parentElement.parentElement
+    var textbox = row.querySelector('input')
+    var form = textbox.parentElement
+    var text = row.querySelector('.text')
 
-  textbox.value = row.querySelector('.text').textContent
-  text.className = text.className.replace('show', 'hidden')
-  form.className = form.className.replace('hidden', 'show')
-  textbox.focus()
-  textbox.select()
-}
+    textbox.value = row.querySelector('.text').textContent
+    text.className = text.className.replace('show', 'hidden')
+    form.className = form.className.replace('hidden', 'show')
+    textbox.focus()
+    textbox.select()
+  }
 
-// If Enter was pressed, update woof in database,
-// If Escape was pressed, hide the textbox
-function editWoof (event) {
-  var row = this.parentElement.parentElement.parentElement
-  var textbox = row.querySelector('input')
-  var form = textbox.parentElement
-  var text = row.querySelector('.text')
+  // If Enter was pressed, update woof in database,
+  // If Escape was pressed, hide the textbox
+  function editWoof (event) {
+    var row = this.parentElement.parentElement.parentElement
+    var textbox = row.querySelector('input')
+    var form = textbox.parentElement
+    var text = row.querySelector('.text')
 
-  if (event.keyCode === 13) {
-    // Enter key pressed
-    wooferdb.updateWoofInDatabase(row.id, textbox.value)
-  } else if (event.keyCode === 27) {
-    // Escape key pressed
+    if (event.keyCode === 13) {
+      // Enter key pressed
+      wooferdb.updateWoofInDatabase(row.id, textbox.value)
+    } else if (event.keyCode === 27) {
+      // Escape key pressed
+      form.className = form.className.replace('show', 'hidden')
+      text.className = text.className.replace('hidden', 'show')
+    }
+  }
+
+  // Update the woof text in a row on the page
+  function updateWoofRow (woofKey, woof) {
+    var row = document.getElementById(woofKey)
+    var form = row.querySelector('input').parentElement
+    var text = row.querySelector('.text')
     form.className = form.className.replace('show', 'hidden')
+    row.querySelector('.text').textContent = woof.text
     text.className = text.className.replace('hidden', 'show')
   }
-}
 
-// Update the woof text in a row on the page
-function updateWoofRow (woofKey, woof) {
-  var row = document.getElementById(woofKey)
-  var form = row.querySelector('input').parentElement
-  var text = row.querySelector('.text')
-  form.className = form.className.replace('show', 'hidden')
-  row.querySelector('.text').textContent = woof.text
-  text.className = text.className.replace('hidden', 'show')
-}
+  // Remove a woof row from the page
+  function deleteWoofRow (woofKey) {
+    var row = document.getElementById(woofKey)
+    row.parentElement.removeChild(row)
+  }
 
-// Remove a woof row from the page
-function deleteWoofRow (woofKey) {
-  var row = document.getElementById(woofKey)
-  row.parentElement.removeChild(row)
-}
+  // Remove the clicked woof from the database
+  function deleteWoof () {
+    var row = this.parentElement.parentElement
+    wooferdb.deleteWoofFromDatabase(row.id)
+  }
 
-// Remove the clicked woof from the database
-function deleteWoof () {
-  var row = this.parentElement.parentElement
-  wooferdb.deleteWoofFromDatabase(row.id)
-}
+  // Event listeners to add a new woof
+  woofCreate.addEventListener('click', createWoof)
+  woofText.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) createWoof()
+  })
 
-// Event listeners to add a new woof
-woofCreate.addEventListener('click', createWoof)
-woofText.addEventListener('keypress', function (event) {
-  if (event.keyCode === 13) createWoof()
-})
+  window.updateWoofRow = updateWoofRow
+  window.deleteWoofRow = deleteWoofRow
+  window.addWoofRow = addWoofRow
+})()
